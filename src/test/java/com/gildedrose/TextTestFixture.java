b76e0506164702ee2409b5e7e165b94e968960ec
xbl <blxie@toughtworks.com>
2019-09-19 09:15:41 +0800

    refactor ready

--- src/test/java/com/gildedrose/TextTestFixture.java
+++ src/test/java/com/gildedrose/TextTestFixture.java
@@ -19,10 +19,6 @@ public class TextTestFixture {
         GildedRose app = new GildedRose(items);
 
         int days = 2;
-        if (args.length > 0) {
-            days = Integer.parseInt(args[0]) + 1;
-        }
-
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
